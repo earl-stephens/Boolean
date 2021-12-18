a947ca10e042a63771ab1672988d55368eff3c6a
earl-stephens <stephensearl@hotmail.com>
2021-12-17 20:23:49 -0700

    Wrote code to demonstrate the use of boolean data type

--- src/application/App.java
+++ src/application/App.java
@@ -3,8 +3,19 @@ package application;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Boolean is a primitive variable type
+		boolean condition1 = false;
+		System.out.println("Condition 1: " + condition1);
+		
+		//Can set the boolean variable to an expression
+		boolean condition2 = 4 < 5;
+		System.out.println("Condition 2: " + condition2);
 
+		boolean condition3 = 8 < 5;
+		System.out.println("Condition 2: " + condition3);
+		
+		boolean condition4 = 4 == 5;
+		System.out.println("Condition 2: " + condition4);
 	}
 
 }
